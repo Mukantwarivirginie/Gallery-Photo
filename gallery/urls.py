7d60caad2a44a7c0e22ff57366e43fc63c026c06
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-14 17:24:27 +0200

    added name gallery in urly.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^today/$',views.gallery_of_day,name='galleryToday')
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,16 @@
+import datetime as dt
 from django.shortcuts import render
 from django.http  import HttpResponse
-
-
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to the Gallery-Photo')
+    def gallery_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
