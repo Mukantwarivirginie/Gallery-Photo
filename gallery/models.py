f77ef26cc785143d21dc40eaf857875ab09c3d2d
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-15 16:39:43 +0200

    added some in models.py

--- gallery/models.py
+++ gallery/models.py
@@ -1,29 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+
+
 class Location(models.Model):
-    city = models.CharField(max_length =30)
-    country = models.ImageField(upload_to='photos/') 
+    city = models.CharField(max_length =30,blank=True)
+    country = models.ImageField(upload_to='photos/',blank=True) 
     def __str__(self):
         return self.country
     
 class Category(models.Model):
-    name  = models.CharField(max_length =30)
+    name  = models.CharField(max_length =30,blank=True)
     def __str__(self):
         return self.category
+    
+
+
 
 class Image(models.Model):
-    image_name = models.CharField(max_length =30)
-    picture = models.ImageField(upload_to='photos/') 
-    description= models.TextField()
-    location = models.CharField(max_length =30)
-    # location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    image_name = models.CharField(max_length =30,blank=True)
+    picture = models.ImageField(upload_to='photos/',blank=True) 
+    description= models.TextField(blank=True)
+    
+    location = models.ForeignKey(Location,blank=True)
+    category = models.ForeignKey(Category,blank=True)
     def __str__(self):
         return self.image_name
     class Meta:
         ordering = ['image_name']
 
 
-
-    
