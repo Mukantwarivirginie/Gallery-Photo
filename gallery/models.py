9d8d7bfadde446cfb0220e894da998412172b053
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-15 14:19:29 +0200

    added image name

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    picture = models.ImageField(upload_to='photos/') 
+    description= models.TextField()
+    # location = models.ForeignKey(Location)
+    # category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.image_name
+    class Meta:
+        ordering = ['image_name']
