7a6109c6f66ce2f9dd476c5db1b0976ed408a95f
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-15 11:07:01 +0200

    added render templatefile in views.py gallery

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render,redirect
 import datetime as dt
 from django.shortcuts import render
 from django.http  import HttpResponse
@@ -49,3 +50,20 @@ def convert_dates(dates):
      def gallery_of_day(request):
     date = dt.date.today()
     return render(request, 'all-gallery/today-gallery.html', {"date": date,}) 
+
+    # View Function to present news from past days
+def past_days_gallery(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'all-gallery/past-gallery.html', {"date": date})
