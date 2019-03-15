39aa452879ed990416b6694f266e6fc89a09a58f
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-15 10:47:17 +0200

    added welcome to Gallery Photos in html

--- gallery/views.py
+++ gallery/views.py
@@ -3,31 +3,45 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 # Create your views here.
 def welcome(request):
+    return render(request, 'welcome.html')
     return HttpResponse('Welcome to the Gallery-Photo')
-    def gallery_of_day(request):
-    date = dt.date.today()
+def gallery_of_day(request):
+      date = dt.date.today()
+
+def past_days_gallery(request,past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+        try:
+        # Converts data from the string Url
+           date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+        except ValueError:
+        # Raise 404 error when ValueError is thrown
+          raise Http404()
+
+
 
  # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
+        day = convert_dates(date)
+        html = f'''
+            <html>
+                <body>
+                    <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
 
-    html = f'''
-        <html>
-            <body>
-                  <h1>Gallery for {day} {date.day}-{date.month}-{date.year}</h1>
+                    
+                </body>
+            </html>
+                '''
+        return HttpResponse(html)
 
-                # <h1> {date.day}-{date.month}-{date.year}</h1>
-             </body>
-        </html>
-            '''
-    return HttpResponse(html)
-
-    def convert_dates(dates):
+def convert_dates(dates):
 
     # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+      day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+      days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
 
     # Returning the actual day of the week
-    day = days[day_number]
-    return day
+      day = days[day_number]
+      return day
